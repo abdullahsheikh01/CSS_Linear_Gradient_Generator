e78b4482c6431e3de7aba320b7a4f49a6577edfa
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-09-15 15:32:19 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+let finalButton = document.getElementById('final-button');
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let direction = "to right";
+function colorGenerator() {
+    let hchrachters = "0123456789abcdef";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        let md = Math.random() * 16;
+        color = color + hchrachters[Math.floor(md)];
+    }
+    ;
+    return color;
+    ;
+}
+let currentFirstColor = "#eef2f3";
+let currentSecondColor = "#8e9eab";
+function firstColor() {
+    currentFirstColor = colorGenerator();
+    if (button1) {
+        button1.innerHTML = currentFirstColor;
+    }
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+function secondColor() {
+    currentSecondColor = colorGenerator();
+    if (button2) {
+        button2.innerHTML = currentSecondColor;
+    }
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+function allColor() {
+    firstColor();
+    secondColor();
+}
+function directionToRight() {
+    let direction = "to right";
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+;
+function directionToLeft() {
+    direction = "to left";
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+function directionToTop() {
+    direction = "to top";
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+function directionToBottom() {
+    direction = "to bottom";
+    document.body.style.backgroundImage = `linear-gradient(${direction},${currentFirstColor},
+    ${currentSecondColor})`;
+}
+;
+let fCb = button1?.addEventListener('click', firstColor);
+let sCb = button2?.addEventListener('click', secondColor);
+let fB = finalButton?.addEventListener("click", allColor);
+let dButtontoRight = document.getElementById("button-toright");
+let toR = dButtontoRight?.addEventListener('click', directionToRight);
+let dButtontoLeft = document.getElementById("button-toleft");
+let toL = dButtontoLeft?.addEventListener('click', directionToLeft);
+let dButtontoTop = document.getElementById("button-totop");
+let toT = dButtontoTop?.addEventListener('click', directionToTop);
+let dButtontoBottom = document.getElementById("button-tobottom");
+let ToB = dButtontoBottom?.addEventListener('click', directionToBottom);
---
+++ script2.js
@@ -0,0 +1,6 @@
+// var a = document.getElementById("para").innerHTML;
+// console.log(a);
+// document.write(a)
+
+let b = document.querySelector('p');
+console.log(b);
